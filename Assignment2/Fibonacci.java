b18acb896eb5d79151c4e2b16305fd5015d72050
goLdnblack <jesusgabisanchez@gmail.com>
2023-01-18 20:12:54 -0500

    Assignment 2

---
+++ Assignment2/Fibonacci.java
@@ -0,0 +1,23 @@
+public class Fibonacci
+{
+	static long fibonacci(int n)
+	{
+		// Base case for recursion
+		if (n == 0)
+			return 0;
+
+		// First two in sequence
+		if (n <= 2)
+			return 1;
+
+		// Return nth term
+		return fibonacci(n - 2) + fibonacci(n - 1);
+	}
+
+	public static void main(String[] args)
+	{
+		int n = 10;
+		
+		System.out.println("The term " + n +" of the Fibonacci sequence is " + fibonacci(n));
+	}
+}
